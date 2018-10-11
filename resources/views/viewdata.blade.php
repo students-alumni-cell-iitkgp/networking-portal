510a7e294623493e8258b22a541fbe7b8d252d62
MaxAsif <asifiqbal7.iitkgp@gmail.com>
2018-10-11 19:39:09 +0530

    Added clipboard.js to viewdata blade

--- resources/views/viewdata.blade.php
+++ resources/views/viewdata.blade.php
@@ -6,159 +6,167 @@
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.0/clipboard.min.js"></script>
 
   <link rel="stylesheet" href="http://cdn.datatables.net/1.10.2/css/jquery.dataTables.min.css">
   <script type="text/javascript" src="http://cdn.datatables.net/1.10.2/js/jquery.dataTables.min.js"></script>
 
 
   <script type="text/javascript" charset="utf-8">
-    $(document).ready(function() {
+  $(document).ready(function() {
+    var clipboard = new ClipboardJS('.btn');
 
 
     $('#example').DataTable(
-    {
-     "columns": [
+      {
+        "columns": [
 
-     { "searchable": false },
-     null,
-     null,
-     null,
-     null,
-     null,
-     { "searchable": false },
-     { "searchable": false },
-     null,
-     { "searchable": false },
-     ]
-   });
-   document.getElementById("mySidenav").style.width = "250px";
-   document.getElementById("main").style.marginLeft = "250px";
- } );
+          { "searchable": false },
+          null,
+          null,
+          null,
+          null,
+          null,
+          { "searchable": false },
+          { "searchable": false },
+          null,
+          { "searchable": false },
+        ]
+      });
+      document.getElementById("mySidenav").style.width = "250px";
+      document.getElementById("main").style.marginLeft = "250px";
+    } );
 
- function copy_mail(element) {
+    function copy_mail(element) {
 
- var $temp = $("<input>");
- $("body").append($temp);
- $temp.val($(element).text()).select();
- document.execCommand("copy");
- $temp.remove();
- alert("Emails have been succesfully copied!");
-}
-</script>
-</head>
-<body>
-  @include('layouts.navbar')
+      var $temp = $("<input>");
+      $("body").append($temp);
+      $temp.val($(element).text()).select();
+      document.execCommand("copy");
+      $temp.remove();
+      alert("Emails have been succesfully copied!");
+    }
+    </script>
+  </head>
+  <body>
+    @include('layouts.navbar')
 
 
 
-  <div class="container">
-    @if($message!='')
-    <div class="alert alert-success">
-      <strong>Message :</strong>{{$message }}
-    </div>
-    <br>
-    @endif
-    @if (session('message'))
-    <div class="alert alert-success">
-      <strong>Message : {{ session('message') }}</strong>
-    </div>
-    @endif
-    @if (session('Error'))
-  <div class="alert alert-danger">
-    <strong>Error : {{ session('Error') }}</strong>
-  </div>
-  @endif
-    To <strong>copy</strong> all the emails in the page : <button type="button" class="btn btn-primary" id="copy_mail" onclick="copy_mail('.email')">COPY</button>
-    <br>
-    <table id="example" class="display" cellspacing="0" width="100%">
-      <thead>
-       <tr>
-        <th>ID</th>
-        <th>ALUMNI</th>
-        <th>EMAIL</th>
-        <th>COMPANY</th>
-        <th>DESIGNATION</th>
-        <th>TAGS</th>
-        <th>ADD TAG TO ALUM</th>
-        <th>DELETE TAG</th>
-        <th>Year</th>
-        <th>Edit Data</th>
-      </tr>
-    </thead>
-    <tbody>
-      @foreach($alumni as $alum)
-      <tr>
-        <td>{{$alum['id']}}</td>
-        <td><a href="{{url('/profile/'.$alum['id'])}}">{{$alum['name']}}</a></td>
-        <td class="email">{{$alum['email'].' '}}</td>
-        <td>{{$alum['industry']}}</td>
-        <td>{{$alum['designation']}}</td>
-        <td><?php
-        $tags_a = App\Addtag::where('alum_id',$alum['id'])->pluck('tags');
-        foreach ($tags_a as $tag_a)
-        {
-          echo $tag_a.'        ';
-        }
-        ?>
-      </td>
+    <div class="container">
+      @if($message!='')
+        <div class="alert alert-success">
+          <strong>Message :</strong>{{$message }}
+        </div>
+        <br>
+      @endif
+      @if (session('message'))
+        <div class="alert alert-success">
+          <strong>Message : {{ session('message') }}</strong>
+        </div>
+      @endif
+      @if (session('Error'))
+        <div class="alert alert-danger">
+          <strong>Error : {{ session('Error') }}</strong>
+        </div>
+      @endif
+      To <strong>copy</strong> all the emails in the page : <button type="button" class="btn btn-primary" id="copy_mail" onclick="copy_mail('.email')">COPY</button>
+      <br>
+      <table id="example" class="display" cellspacing="0" width="100%">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>ALUMNI</th>
+            <th>EMAIL</th>
+            <th>COMPANY</th>
+            <th>DESIGNATION</th>
+            <th>TAGS</th>
+            <th>ADD TAG TO ALUM</th>
+            <th>DELETE TAG</th>
+            <th>Year</th>
+            <th>Edit Data</th>
+          </tr>
+        </thead>
+        <tbody>
+          @foreach($alumni as $alum)
+            <tr>
+              <td>{{$alum['id']}}</td>
+              <td><a href="{{url('/profile/'.$alum['id'])}}">{{$alum['name']}}</a></td>
+              <td class="email">
+                <span id="copy{{ $alum['id'] }}"> {{$alum['email'].' '}} </span>
+                @if($alum['email'])
+                <button class="btn" data-clipboard-target="#copy{{ $alum['id']}}">
+                  <img src="https://clipboardjs.com/assets/images/clippy.svg" alt="Copy to clipboard" style="height: 18px; width: 18px;">
+                </button>
+                @endif
+              </td>
+                <td>{{$alum['industry']}}</td>
+                <td>{{$alum['designation']}}</td>
+                <td><?php
+                $tags_a = App\Addtag::where('alum_id',$alum['id'])->pluck('tags');
+                foreach ($tags_a as $tag_a)
+                {
+                  echo $tag_a.'        ';
+                }
+                ?>
+              </td>
 
-      <td>
-        <form action="{{ url('/assigntag/'.$alum['id'])}}" method="post">
-          {{csrf_field()}}
-          <select name="tag" id="{{$alum['id']}}" class="form-control">
-            @foreach($tags as $tag)
-            <option value="{{$tag['tagname']}}">{{$tag['tagname']}}</option>
-            @endforeach
-          </select>
+              <td>
+                <form action="{{ url('/assigntag/'.$alum['id'])}}" method="post">
+                  {{csrf_field()}}
+                  <select name="tag" id="{{$alum['id']}}" class="form-control">
+                    @foreach($tags as $tag)
+                      <option value="{{$tag['tagname']}}">{{$tag['tagname']}}</option>
+                    @endforeach
+                  </select>
 
-           <div class="form-group">
-          <button type="submit" style="background:none; border:none;">
-           <span class="glyphicon glyphicon-plus"></span>
-         </button>
-         </div>
-       </form>
-     </td>
+                  <div class="form-group">
+                    <button type="submit" style="background:none; border:none;">
+                      <span class="glyphicon glyphicon-plus"></span>
+                    </button>
+                  </div>
+                </form>
+              </td>
 
-     <td>
-      <form action="{{ url('/taggdelete/'.$alum['id'])}}" method="POST">
-        {{csrf_field()}}
-        <?php
-        $tags_a = App\Addtag::where('alum_id',$alum['id'])->get();
-        ?>
-        <select name="tagd" id="{{$alum['id']}}" class="form-control">
-          @foreach($tags_a as $tag_a)
-          <option value="{{$tag_a['id']}}">{{$tag_a['tags']}}</option>
+              <td>
+                <form action="{{ url('/taggdelete/'.$alum['id'])}}" method="POST">
+                  {{csrf_field()}}
+                  <?php
+                  $tags_a = App\Addtag::where('alum_id',$alum['id'])->get();
+                  ?>
+                  <select name="tagd" id="{{$alum['id']}}" class="form-control">
+                    @foreach($tags_a as $tag_a)
+                      <option value="{{$tag_a['id']}}">{{$tag_a['tags']}}</option>
+                    @endforeach
+                  </select>
+                  @if(sizeof($tags_a)>0)
+                    <button type="submit" style="background:none; border:none;">
+                      <span class="glyphicon glyphicon-trash"></span>
+                    </button>
+                  @endif
+                </form>
+              </td>
+
+              <td>{{$alum['year']}}</td>
+              <td>
+                <a href="{{url('/editalum/'.$alum['id'])}}">
+                  <span class="glyphicon glyphicon-edit"></span>
+                </a>
+
+              </td>
+
+            </tr>
           @endforeach
-        </select>
-        @if(sizeof($tags_a)>0)
-        <button type="submit" style="background:none; border:none;">
-          <span class="glyphicon glyphicon-trash"></span>
-        </button>
-        @endif
-      </form>
-    </td>
-
-    <td>{{$alum['year']}}</td>
-    <td>
-      <a href="{{url('/editalum/'.$alum['id'])}}">
-        <span class="glyphicon glyphicon-edit"></span>
-      </a>
-
-    </td>
-
-  </tr>
-  @endforeach
-</tbody>
-</table>
-</div>
+        </tbody>
+      </table>
+    </div>
 
 
-<script type="text/javascript">
-  // For demo to fit into DataTables site builder...
-  $('#example')
-  .removeClass( 'display' )
-  .addClass('table table-striped table-bordered');
-</script>
+    <script type="text/javascript">
+    // For demo to fit into DataTables site builder...
+    $('#example')
+    .removeClass( 'display' )
+    .addClass('table table-striped table-bordered');
+  </script>
 </body>
 </html>
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -11,8 +11,8 @@ class ComposerStaticInit9ec0ee6e7472e1834af503169b21b58a
         '667aeda72477189d0494fecd327c3641' => __DIR__ . '/..' . '/symfony/var-dumper/Resources/functions/dump.php',
         '2c102faa651ef8ea5874edb585946bce' => __DIR__ . '/..' . '/swiftmailer/swiftmailer/lib/swift_required.php',
         '5255c38a0faeba867671b61dfda6d864' => __DIR__ . '/..' . '/paragonie/random_compat/lib/random.php',
-        'e7223560d890eab89cda23685e711e2c' => __DIR__ . '/..' . '/psy/psysh/src/Psy/functions.php',
         '6124b4c8570aa390c21fafd04a26c69f' => __DIR__ . '/..' . '/myclabs/deep-copy/src/DeepCopy/deep_copy.php',
+        'e7223560d890eab89cda23685e711e2c' => __DIR__ . '/..' . '/psy/psysh/src/Psy/functions.php',
         'f0906e6318348a765ffb6eb24e0d0938' => __DIR__ . '/..' . '/laravel/framework/src/Illuminate/Foundation/helpers.php',
         '58571171fd5812e6e447dce228f52f4d' => __DIR__ . '/..' . '/laravel/framework/src/Illuminate/Support/helpers.php',
     );
@@ -107,8 +107,8 @@ class ComposerStaticInit9ec0ee6e7472e1834af503169b21b58a
         'phpDocumentor\\Reflection\\' => 
         array (
             0 => __DIR__ . '/..' . '/phpdocumentor/reflection-common/src',
-            1 => __DIR__ . '/..' . '/phpdocumentor/type-resolver/src',
-            2 => __DIR__ . '/..' . '/phpdocumentor/reflection-docblock/src',
+            1 => __DIR__ . '/..' . '/phpdocumentor/reflection-docblock/src',
+            2 => __DIR__ . '/..' . '/phpdocumentor/type-resolver/src',
         ),
         'XdgBaseDir\\' => 
         array (
